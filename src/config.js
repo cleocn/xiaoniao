9c62a88826be580ddd1e0c17bc364778424e2882
ljx <cleocn@qq.com>
2017-12-08 20:29:52 +0800

    基本流程OK版本

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@
 */
 // const urlApi = 'https://api.4dz.club:3000/'
 // const urlApi = 'http://service-ospailfo-1255547872.ap-guangzhou.apigateway.myqcloud.com/'
-// const urlApi = 'http://139.199.9.175:3000'
-const urlApi = 'http://localhost:3000'
+const urlApi = 'http://139.199.9.175:3000'
+// const urlApi = 'http://localhost:3000'
 
 module.exports = {
   urlBase: urlApi,
