bbbbba5a562a8ab7f95c9eae9c0a991c1e6fa492
ljx <cleocn@qq.com>
2017-12-10 11:31:11 +0800

    修复转发，增加cdn

--- src/config.js
+++ src/config.js
@@ -3,11 +3,14 @@
 */
 // const urlApi = 'https://api.4dz.club:3000/'
 // const urlApi = 'http://service-ospailfo-1255547872.ap-guangzhou.apigateway.myqcloud.com/'
-const urlApi = 'http://139.199.9.175:3000'
+// const urlApi = 'http://139.199.9.175:3000'
 // const urlApi = 'http://localhost:3000'
+const urlApi = 'https://api.hnn7.com'
+const cdnDomain = 'https://cdn.hnn7.com'
 
 module.exports = {
   urlBase: urlApi,
+  cdnDomain: cdnDomain,
   apiList: {
     upload: urlApi + '/upload',
     recommended: {
